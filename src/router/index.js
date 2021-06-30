7d0c706ce2f2e61c91f86686047fb96250fa6c96
Keeks78 <paul.grandvaux@outlook.fr>
2021-06-30 14:19:14 +0200

    modif

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,15 @@
 /* eslint-disable */
 import Vue from "vue";
 import Router from "vue-router";
-import Index from "@/views/Index"
+import Login from "../Login";
 
 Vue.use(Router)
 
 const router = new Router({
     mode: "history",
     routes: [{
-        path: "/",
-        name: "Index",
-        component: Index,
-        meta: {
-            requiresAuth: true
-        },
-    }, {
         path: "/login",
         name: "Login",
         component: Login,
-        meta: {
-            loggedin: true
-        }
     }, ]
 });
