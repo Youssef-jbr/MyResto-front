9a9caf55cc875595c45628a7f265c1ecf628b6fe
KevinCeyland <kevin-78440@hotmail.fr>
2021-06-30 16:51:58 +0200

    Réarengement de la page carte

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
+  <b-navbar toggleable="lg" type="dark" variant="dark">
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item :to="{ name: 'Carte' }">Carte</b-nav-item>
-        <b-nav-item :to="{ name: 'Commander' }">Commander</b-nav-item>
-        <b-nav-item :to="{ name: 'TableauDeBord' }">Admin Panel</b-nav-item>
-        <b-nav-item :to="{ name: 'Login' }">Authentification</b-nav-item>
-        <b-nav-item :to="{ name: 'Deconnexion' }"> <b-icon icon="power" aria-hidden="true"></b-icon></b-nav-item>
+        <b-nav-item class="text-dark" :to="{ name: 'Carte' }">Carte</b-nav-item>
+        <b-nav-item class="text-dark" :to="{ name: 'Commander' }">Commander</b-nav-item>
+        <b-nav-item class="text-dark" :to="{ name: 'TableauDeBord' }">Admin Panel</b-nav-item>
+        <b-nav-item class="text-dark" :to="{ name: 'Login' }">Authentification</b-nav-item>
+        <b-nav-item class="text-dark" :to="{ name: 'Deconnexion' }"> <b-icon icon="power" aria-hidden="true"></b-icon></b-nav-item>
       </b-navbar-nav>
     </b-collapse>
 
